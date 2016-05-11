6ccec5e13e86df8975f3170bb05c0c5a0918ae33
Kool Havis <havisk@gmail.com>
2016-05-11 02:39:45 -0400

    use each construct in an array of immages

--- image_share.js
+++ image_share.js
@@ -1,9 +1,28 @@
 if (Meteor.isClient){
+  var img_data = [
+    {
+      img_src: "mygear.jpg",
+      img_alt: "my music gear"
+    },
 
+    {
+      img_src: "kool.jpg",
+      img_alt: "pic of me"
+    },
+
+    {
+      img_src: "sip.jpg",
+      img_alt: "logo"
+    },
+
+    ]
+
+  Template.images.helpers({images: img_data});
 }
 
 if (Meteor.isServer){
 	Meteor.startup(function(){
 		// code to run on server at startup
+    console.log(" I am the server")
 	})
 }
